4b0ceabff09cc715551562a76821dbf90b776ff9
Dave <Davemo-coderpersonal@hotmail.com>
2020-11-23 07:06:36 +0800

    ThreadLocal基本理解

---
+++ Week_04/src/com/thread/study/ThreadLocalTest.java
@@ -0,0 +1,26 @@
+package com.thread.study;
+
+public class ThreadLocalTest {
+    public static ThreadLocal<String> val = new ThreadLocal<>();
+    public void test() throws InterruptedException {
+        val.set("Hello world");
+        String v = val.get();
+        System.out.println("value v:" + v);
+
+        new Thread(() -> {
+            String v2 = val.get();
+            //线程级别的共享变量，跨线程无效
+            System.out.println("value v2:" + v2);
+            val.set("java");
+            v2 = val.get();
+            System.out.println("new v2:" + v2);
+            System.out.println("done");
+        }).start();
+        Thread.sleep(5000L);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new ThreadLocalTest().test();
+        //局部变量，栈封闭
+    }
+}
---
+++ Week_04/src/com/thread/study/ThreadPoolTest.java
@@ -0,0 +1,5 @@
+package com.thread.study;
+
+public class ThreadPoolTest {
+    //todo
+}
